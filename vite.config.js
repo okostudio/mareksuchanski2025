aa5096902325fa986a187dfafbe8f109b4e3e2d0
okostudio <marek.suchanski@gmail.com>
2024-11-05 17:45:46 +1100

    font issue fix attemt 001

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      $fonts: resolve('./public/fonts')
+    }
+  }
 })
+
+
