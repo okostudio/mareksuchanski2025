8999f0e302fb1cd69e9a3b6497cb24198f4c3bdf
okostudio <marek.suchanski@gmail.com>
2024-11-07 18:31:09 +1100

    responsive, overflow fix

--- src/components/Testemonials.jsx
+++ src/components/Testemonials.jsx
@@ -15,25 +15,26 @@ const Testemonials = () => {
                     <div className="quotes">
                         <div className="logo">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1303 372"><path d="M234.1,64.2v299h-74.5v-299H0V0h393.7v64.2H234.1L234.1,64.2z"></path><path d="M540.7,169c-12.1-3-17.6-4-31.7-4c-58.4,0-97.2,23.8-97.2,85v113.3h-70.5V97.1h70.5v47c22.6-34.9,56.4-48.6,99.7-48.6c12.1,0,21.6,1.5,29.2,3.5L540.7,169L540.7,169z"></path><path d="M851.9,363.2h-70.5v-33.4c-19.6,22.3-51.4,39-95.2,39c-70,0-114.8-47.1-114.8-120.9V97.1h70.5v138.6c0,45,21.6,70.8,66.5,70.8c44.8,0,73-28.3,73-75.4V97.1h70.5V363.2z"></path><path d="M1121.2,262.5l59.9,25.8c-17.6,44-71.5,80.4-141.5,80.4c-91.1,0-156.6-56.2-156.6-138.6s64.4-138.1,156.1-138.1c91.6,0,146.5,59.7,146.5,135.6v22.3H951c8.6,38.4,43.3,61.2,88.6,61.2C1078.9,311.1,1109.1,291.8,1121.2,262.5L1121.2,262.5zM953.5,201.8h162.6c-4-24.8-33.7-53.1-79.5-53.1C997.8,148.7,964.6,166.9,953.5,201.8L953.5,201.8z"></path><path d="M1204.1,291.7l80.4-18.8l18.4,79.8l-80.4,18.8L1204.1,291.7z"></path></svg>
+                            <div className="description p3">
+                                “It’s great to know we have a team behind us who can produce such great content, that we can really rely on.”
+                            </div>
                         </div>
                         <div className="logo saatchi">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 125 110.5"><path d="M42.5,56.1c-7.1-8.3-11-13.6-14.8-13.6-7.4,0-17.4,8.5-17.4,20.8.3,9.4,4.3,18.3,11.1,24.8,7.7,7.9,18.2,12.3,29.2,12.3,9.9,0,19.5-3.3,27.2-9.6-12.5-10.8-24.2-22.5-35.2-34.9M44.1,110c-24.8,0-42.3-16.8-42.3-40.4s9.6-26.9,25.8-31.5c-4.1-4.8-6.5-10.8-6.8-17.1C20.8,9,30.5.4,44.4.4c5.8.2,11.5,1.5,16.8,3.7,1.2,0,2.5-1.4,3.5-1.4s1.4,1.2,1.5,3.9l.6,13.3v.5c0,1.7-.8,2.6-1.7,2.6-2.2,0-2.2-3.7-5-7.7-3.8-6.6-10.7-10.7-18.2-11-6.4-.4-11.9,4.5-12.3,10.9,0,.4,0,.9,0,1.3,0,10.2,18.8,30.4,55.2,64.8,2.3-2.9,4-6.2,5-9.8,2.1-9.1-.8-11.2-6.8-13.8-3.7-1.6-8.3-.6-8.3-3.2s1.4-1.9,4-1.9,7.6.7,13.6.6c8.1-.1,16.4-1.6,16.7.8.4,4.5-11.1,2.4-12.3,9.3-1,6,.4,9.7-7.5,21.8,13.9,12,22.4,18.7,27.2,18.7,1.8-.2,3.5-.6,5.2-1.1.9,0,1.5.7,1.5,1.5,0,2.2-4.8,5.4-12.4,5.4s-17.4-5.7-29.2-15.3c-9.8,10.2-23.5,15.9-37.7,15.6" /></svg>
+                            <div className="description p3">
+                                “It’s great to know we have a team behind us who can produce such great content, that we can really rely on.”
+                            </div>
                         </div>
                         <div className="logo">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 159 102"><path d="M25.243 0H0v21.61h25.298c15.986 0 28.992 13.078 28.992 29.152S41.284 79.913 25.298 79.913H0v21.611h25.243c27.838 0 50.485-22.771 50.485-50.762C75.728 22.772 53.08 0 25.243 0ZM133.706 79.113c-15.546 0-28.196-12.716-28.196-28.348 0-15.63 12.647-28.35 28.196-28.35h25.295V0h-25.242c-27.838 0-50.486 22.771-50.486 50.762 0 27.99 22.648 50.762 50.486 50.762h25.242V79.11h-25.295v.003Z"></path></svg>
+                            <div className="description p3">
+                                “It’s great to know we have a team behind us who can produce such great content, that we can really rely on.”
+                            </div>
                         </div>
 
-                        <div className="description p3">
-                            “It’s great to know we have a team behind us who can produce such great content, that we can really rely on.”
-                        </div>
 
-                        <div className="description p3">
-                            “It’s great to know we have a team behind us who can produce such great content, that we can really rely on.”
-                        </div>
 
-                        <div className="description p3">
-                            “It’s great to know we have a team behind us who can produce such great content, that we can really rely on.”
-                        </div>
+
                     </div>
                 </div>
             </div>
